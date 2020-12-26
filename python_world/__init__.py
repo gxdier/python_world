ea2717e3f507ac1a97db5850a30ab4fcc8df54c9
GXDier <gaetan.disdier@gmail.com>
2020-12-26 19:14:54 +0000

    Update __init__.py

--- python_world/__init__.py
+++ python_world/__init__.py
@@ -1,2 +1,7 @@
 name = "python_world"
 
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+from logging import NullHandler
+
+logging.getLogger(__name__).addHandler(NullHandler())
