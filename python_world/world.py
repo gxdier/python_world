a3fb07c8cc2f0c5202269ce2dea04d67003de039
d.allonsius <allonsius.damien@hotmail.fr>
2021-02-05 21:05:24 +0100

    corrected world.py strings

--- python_world/world.py
+++ python_world/world.py
@@ -2,10 +2,10 @@ import logging
 
 def world():
 	
-	logging.debug('Debug mon minipougne)
+	logging.debug('Debug mon minipougne')
 	logging.info('Info mon minipougne')
-	logging.warning('Warning mon minipougne)
-	logging.error('Error mon minipougne)
-	logging.critical('Critical mon minipougne)
+	logging.warning('Warning mon minipougne')
+	logging.error('Error mon minipougne')
+	logging.critical('Critical mon minipougne')
 	
 	return "world"
