81391250d764a8bf3e9d68a509ed883e8df434eb
GXDier <gaetan.disdier@gmail.com>
2021-02-06 15:02:59 +0000

    Create world1.py

---
+++ python_world/world1.py
@@ -0,0 +1,11 @@
+import logging
+
+
+def world():
+    
+    logging.debug('Debug mon minipougne')
+    logging.info('Info mon minipougne')
+    logging.warning('Warning mon minipougne')
+    logging.error('Error mon minipougne')
+    logging.critical('Critical mon minipougne')
+    return "world"
